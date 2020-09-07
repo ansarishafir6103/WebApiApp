dbdc140cef7ed6759f5f72aade246f425b0b4aa3
ansarishafir6103 <ansarishafir@outlook.com>
2020-09-07 18:27:55 +0530

    First Intial commit

---
+++ WebApi/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    /// <summary>
+    /// Represents one specific person 
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// ID from  SQL 
+        /// </summary>
+        public int Id { get; set; } = 0;
+        /// <summary>
+        /// the user's first name. 
+        /// </summary>
+        public string FirstName { get; set; } = "";
+        /// <summary>
+        /// Couldn't you gusess?
+        /// </summary>
+        public string LastName { get; set; } = "";
+    }
+}
